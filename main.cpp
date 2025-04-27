df66570dafa838d809c21cb187646befa29500c4
Luciano Fabrici <lucianofabrici1@gmail.com>
2025-04-27 12:40:27 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//Realizar un programa que muestra por pantalla la leyenda “Hola Nombre Apellido” y dejar el cursor en la siguiente línea. 
+//Por supuesto que Nombre y Apellido debe reemplazarlos.
+
+#include <iostream>
+using namespace std;
+
+int main () {
+cout <<"Hola Luciano Fabrici" << endl;
+return 0;
+
+
+}
